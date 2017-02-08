ae09f8515c05c385e81b9ae29c418da129bdc330
mudrakpatel <mudrakpatel96@gmail.com>
2017-02-08 18:28:01 -0500

    Ex4 45% done

--- MudrakPatel_Lab03_Ex4/Program.cs
+++ MudrakPatel_Lab03_Ex4/Program.cs
@@ -8,8 +8,28 @@ namespace MudrakPatel_Lab03_Ex4
 {
     class Program
     {
+        static int[] intArray = { };
+        static double[] doubleArray = { };
+
         static void Main(string[] args)
         {
+            for (int intIndex = 0; intIndex<=100; intIndex++)
+            {
+                intArray[intIndex] = intIndex;
+                for (double doubleIndex = 0.0; doubleIndex<=100.00; doubleIndex++)
+                {
+                    doubleArray[(int)doubleIndex] = doubleIndex;
+                }
+            }
+
+            intArray.ToList();
+            doubleArray.ToList();
+        }
+
+        //Overloaded DisplayArray method
+        public static void DisplayArray<Type>(Type[] inputArray, int lowIndex, int highIndex)
+        {
+
         }
     }
 }
