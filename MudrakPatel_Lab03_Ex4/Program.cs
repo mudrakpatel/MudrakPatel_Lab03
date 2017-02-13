e7bcf574eefcbe6160b47e4ba03ed21f02ba64b0
mudrakpatel <mudrakpatel96@gmail.com>
2017-02-13 16:40:13 -0500

    Ex 4 almost done

--- MudrakPatel_Lab03_Ex4/Program.cs
+++ MudrakPatel_Lab03_Ex4/Program.cs
@@ -18,8 +18,14 @@ namespace MudrakPatel_Lab03_Ex4
                 intList.Add(index);
                 doubleList.Add((double)index);
             }
-
+            Console.WriteLine("\nAll elements of integer array\n--------------------------------------------------------");
             DisplayArray(intList.ToArray());
+            Console.WriteLine("\nAll elements of double array\n--------------------------------------------------------");
+            DisplayArray(doubleList.ToArray());
+            Console.WriteLine("\nAll elements of integer array from 3rd place to 8th place\n--------------------------------------------------------");
+            DisplayArray(intList.ToArray(), 3, 8);
+            Console.WriteLine("\nAll elements of double array from 3rd place to 8th place\n--------------------------------------------------------");
+            DisplayArray(doubleList.ToArray(), 3, 8);
         }
 
         //Overloaded DisplayArray method
@@ -33,9 +39,19 @@ namespace MudrakPatel_Lab03_Ex4
             Console.WriteLine("-----------------------------------------------s");
         }
 
-        public static void DisplayArray<Type>(Type[] inputCollectionObject, int highIndex, int lowIndex)
+        public static void DisplayArray<Type>(Type[] inputCollectionObject, int lowIndex , int highIndex)
         {
+            var selectedElements = new List<int>();
+            for (int index = lowIndex; index <= highIndex; index++)
+            {
+                selectedElements.Add(Convert.ToInt32(inputCollectionObject[index]));
+            }
 
+            Console.WriteLine("The elements within the range you specified:\n--------------------------------------------------");
+            foreach (var element in selectedElements)
+            {
+                Console.WriteLine("Element: {0:3}", element);
+            }
         }
     }
 }
